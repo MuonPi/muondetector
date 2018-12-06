8fa8c5a3ffd52ce0205389341be52b483df6a7b8
Marvin <37504638+marvin5300@users.noreply.github.com>
2018-12-06 15:18:24 +0100

    cleared master branch from old files master is now version 1.0.2
    Version 1.0.1 can be found in it's specific branch

--- muondetector-daemon-1.0.0/muondetector.pro
+++
@@ -1,3 +0,0 @@
-SUBDIRS += src
-TEMPLATE = subdirs
-CONFIG += c++11 ordered warn_on release
--- muondetector-daemon-1.0.0/src/apt_cache_search.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-objdump -p $1 | grep NEEDED | awk '{print $2}' > dependencies.tmp
-wait
-while read p; do
-dpkg -S $p | awk '{print $1}' > packages.tmp
-done < dependencies.tmp
-wait
-cat packages.tmp | sed -n '/-dev/p' > packages-dev.tmp
