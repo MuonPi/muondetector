d3965fa8166f3e0884f74d965fe40345bb72ef86
Marvin <37504638+marvin5300@users.noreply.github.com>
2019-02-12 19:37:35 +0100

    fixed conversion from gps to epoch timestamp.
    Just added the offset between gps and unix epoch. There are no further adjustments needed so far, the ublox module automatically adjusted for gps cycle and there were no leapseconds difference. The time is working fine now.

--- muondetector-daemon-1.0.2/src/main/unixtime_from_gps.h
+++ muondetector-daemon-1.0.2/src/main/unixtime_from_gps.h
@@ -3,8 +3,19 @@
 
 #include <chrono>
 #include <time_from_rtc.h>
+#include <QDateTime>
 
 static timespec unixtime_from_gps(int week_nr, long int s_of_week, long int ns /*,int leap_seconds*/) {
+    QDateTime gpsTimeStart(QDate(1980,1,6),QTime(0,0,0,0));
+    quint64 gpsOffsetToEpoch = gpsTimeStart.toMSecsSinceEpoch()/1000;
+    //qDebug() << "week: " << week_nr << " second: " << s_of_week << " ns: " << ns;
+    //qDebug() << "gpsOffsetToEpoch: "<<gpsOffsetToEpoch;
+    quint64 secs = gpsOffsetToEpoch;
+    secs += week_nr*7*24*3600;  // 7 days per week, 24 hours per day and 3600 seconds per hour
+    secs += s_of_week;
+    //qDebug() << "secs: " << secs;
+    //qDebug() << "secs: " << secs;
+    /*
 	struct tm time;
 	time.tm_year = 80;
 	time.tm_mon = 0;
@@ -29,7 +40,7 @@ static timespec unixtime_from_gps(int week_nr, long int s_of_week, long int ns /
 	time.tm_hour = sod / 3600;
 	time.tm_min = sod / 60 - time.tm_hour * 60;
 	time.tm_sec = sod - time.tm_hour * 3600 - time.tm_min * 60/*+leap_seconds*/;
-	mktime(&time);
+    /*mktime(&time);
 
 
 	t_from_rtc(&time);
@@ -37,11 +48,10 @@ static timespec unixtime_from_gps(int week_nr, long int s_of_week, long int ns /
 	time_t secs = t_from_rtc(&time);
 
 	//   printf("GPS time: %s",asctime(&time));
-
+*/
 	struct timespec ts;
 	ts.tv_sec = secs;
-	ts.tv_nsec = ns;
-
+    ts.tv_nsec = ns;
 	//   cout<<secs<<"."<<setw(6)<<setfill('0')<<ns<<" "<<setfill(' ')<<endl;
 
 	return ts;
