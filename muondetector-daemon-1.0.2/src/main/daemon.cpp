2c1865c63ce61335fc7cf323a573d89219ec7eb0
Marvin <37504638+marvin5300@users.noreply.github.com>
2019-03-14 11:38:11 +0100

    rate statistics working within normal parameters
    hope it does not crash anymore...

--- muondetector-daemon-1.0.2/src/main/daemon.cpp
+++ muondetector-daemon-1.0.2/src/main/daemon.cpp
@@ -1231,15 +1231,16 @@ qreal Daemon::getRateFromCounts(quint8 which_rate){
     timespec now;
     timespec_get(&now, TIME_UTC);
     qreal timeInterval = (qreal)(500*(counts->size()-1)+(qreal)msecdiff(now,lastRateInterval));
-    return (sum/timeInterval);
+    qreal rate = sum/timeInterval*1000;
+    return (rate);
 }
 
 void Daemon::onRateBufferReminder(){
-    qreal msecsSinceStart = (qreal)msecdiff(lastRateInterval,startOfProgram);
+    qreal secsSinceStart = 0.001*(qreal)msecdiff(lastRateInterval,startOfProgram);
     qreal xorRate = getRateFromCounts(XOR_RATE);
     qreal andRate = getRateFromCounts(AND_RATE);
-    QPointF xorPoint(msecsSinceStart, xorRate);
-    QPointF andPoint(msecsSinceStart, andRate);
+    QPointF xorPoint(secsSinceStart, xorRate);
+    QPointF andPoint(secsSinceStart, andRate);
     xorRatePoints.append(xorPoint);
     andRatePoints.append(andPoint);
     while (xorRatePoints.size()>rateBufferTime/rateBufferInterval){
