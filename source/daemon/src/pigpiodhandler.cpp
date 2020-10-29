eedf9a59b01cf3466009d7ef7af92af0982b9be7
Daniel Treffenstädt <daniel@treffenstaedt.de>
2020-10-29 09:57:08 +0100

    Autoformatting from qtcreator

--- source/daemon/src/pigpiodhandler.cpp
+++ source/daemon/src/pigpiodhandler.cpp
@@ -33,31 +33,31 @@ inline static T sqr(T x) { return x*x; }
   *slope  = output slope
  */
 void calcLinearCoefficients(int n, quint64 *xarray, qint64 *yarray,
-			    double *offs, double* slope,  int arrayIndex = 0)
+                double *offs, double* slope,  int arrayIndex = 0)
 {
    if (n<3) return;
 
-   long double   sumx = 0.0;                        /* sum of x                      */
-   long double   sumx2 = 0.0;                       /* sum of x**2                   */
-   long double   sumxy = 0.0;                       /* sum of x * y                  */
-   long double   sumy = 0.0;                        /* sum of y                      */
-   long double   sumy2 = 0.0;                       /* sum of y**2                   */
+   long double   sumx = 0.0L;                        /* sum of x                      */
+   long double   sumx2 = 0.0L;                       /* sum of x**2                   */
+   long double   sumxy = 0.0L;                       /* sum of x * y                  */
+   long double   sumy = 0.0L;                        /* sum of y                      */
+   long double   sumy2 = 0.0L;                       /* sum of y**2                   */
 
    int ix=arrayIndex;
    if (ix==0) ix=n-1;
    else ix--;
-   
+
    quint64 offsx=xarray[ix];
    qint64 offsy=yarray[ix];
 //    long long int offsy=0;
 
    int i;
    for (i=0; i<n; i++) {
-          sumx  += xarray[i]-offsx;       
-          sumx2 += sqr(xarray[i]-offsx);  
+          sumx  += xarray[i]-offsx;
+          sumx2 += sqr(xarray[i]-offsx);
           sumxy += (xarray[i]-offsx) * (yarray[i]-offsy);
           sumy  += (yarray[i]-offsy);
-          sumy2 += sqr(yarray[i]-offsy); 
+          sumy2 += sqr(yarray[i]-offsy);
    }
 
 
@@ -72,12 +72,12 @@ void calcLinearCoefficients(int n, quint64 *xarray, qint64 *yarray,
 
    long double m = (n * sumxy  -  sumx * sumy) / denom;
    long double b = (sumy * sumx2  -  sumx * sumxy) / denom;
-   
+
    *slope=(double)m;
    *offs=(double)(b+offsy);
 //    *offs=b;
 //   printf("offsI=%lld  offsF=%f\n", offsy, b);
-   
+
 }
 
 
@@ -85,7 +85,7 @@ void calcLinearCoefficients(int n, quint64 *xarray, qint64 *yarray,
 
 
 /* This is the central interrupt routine for all registered GPIO pins
- * 
+ *
  */
 static void cbFunction(int user_pi, unsigned int user_gpio,
     unsigned int level, uint32_t tick) {
@@ -94,38 +94,38 @@ static void cbFunction(int user_pi, unsigned int user_gpio,
         pigpio_stop(pi);
         return;
     }
-	if (pi != user_pi) {
-		// put some error here for the case pi is not the same as before initialized
-		return;
-	}
-	
+    if (pi != user_pi) {
+        // put some error here for the case pi is not the same as before initialized
+        return;
+    }
+
     QPointer<PigpiodHandler> pigpioHandler = pigHandlerAddress;
 
-   	if (pigpioHandler->isInhibited()) return;
+    if (pigpioHandler->isInhibited()) return;
 
     static uint32_t lastTriggerTick=0;
     static uint32_t lastXorAndTick=0;
     static uint32_t lastTick=0;
     static uint16_t pileupCounter=0;
-    
+
     // look, if the last event occured just recently (less than 100us ago)
     // if so, count the pileup counter up
     // count down if not
     if (tick-lastTick<100) pileupCounter++;
     else if (pileupCounter>0) pileupCounter--;
-    
+
     // if more than 50 pileups happened in a short period of time, leave immediately
     if (pileupCounter>50) return;
-    
+
     try{
-		// allow only registered signals to be processed here
-		// if gpio pin fired which is not in GPIO_PIN list, return immediately
-        auto it=std::find_if(GPIO_PINMAP.cbegin(), GPIO_PINMAP.cend(), 
-		[&user_gpio](const std::pair<GPIO_PIN, unsigned int>& val) {
-			if (val.second==user_gpio) return true;
-			return false;
-		});
-		if (it==GPIO_PINMAP.end()) return;
+        // allow only registered signals to be processed here
+        // if gpio pin fired which is not in GPIO_PIN list, return immediately
+        auto it=std::find_if(GPIO_PINMAP.cbegin(), GPIO_PINMAP.cend(),
+        [&user_gpio](const std::pair<GPIO_PIN, unsigned int>& val) {
+            if (val.second==user_gpio) return true;
+            return false;
+        });
+        if (it==GPIO_PINMAP.end()) return;
 
 /*
         if (user_gpio == GPIO_PINMAP[ADC_READY]) {
@@ -133,7 +133,7 @@ static void cbFunction(int user_pi, unsigned int user_gpio,
             return;
         }
 */
-		QDateTime now = QDateTime::currentDateTimeUtc();
+        QDateTime now = QDateTime::currentDateTimeUtc();
         //qDebug()<<"gpio evt: gpio="<<user_gpio<<"  GPIO_PINMAP[EVT_XOR]="<<GPIO_PINMAP[EVT_XOR];
 //        if (user_gpio == GPIO_PINMAP[EVT_AND] || user_gpio == GPIO_PINMAP[EVT_XOR]){
 
@@ -151,25 +151,25 @@ static void cbFunction(int user_pi, unsigned int user_gpio,
 
         if (user_gpio == GPIO_PINMAP[TIMEPULSE]) {
 //			std::cout<<"Timepulse"<<std::endl;
-			struct timespec ts;
-			clock_gettime(CLOCK_REALTIME, &ts);
-			quint64 timestamp = pigpioHandler->gpioTickOverflowCounter + tick;
-			quint64 t0 = pigpioHandler->startOfProgram.toMSecsSinceEpoch();
-			
-			long double meanDiff=pigpioHandler->clockMeasurementOffset;
-   
-			long double dx=timestamp-pigpioHandler->lastTimeMeasurementTick;
-			long double dy=pigpioHandler->clockMeasurementSlope*dx;
-			meanDiff+=dy;
-			
-			qint64 meanDiffInt=(qint64)meanDiff;
-			double meanDiffFrac=(meanDiff-(qint64)meanDiff);
-			timestamp+=meanDiffInt; // add diff to real time
-			long int ts_sec=timestamp/1000000+(t0/1000);             // conv. us to s
-			long int ts_nsec=1000*(timestamp%1000000)+(t0 % 1000) * 1000000L;
-			ts_nsec+=(long int)(1000.*meanDiffFrac);
+            struct timespec ts;
+            clock_gettime(CLOCK_REALTIME, &ts);
+            quint64 timestamp = pigpioHandler->gpioTickOverflowCounter + tick;
+            quint64 t0 = pigpioHandler->startOfProgram.toMSecsSinceEpoch();
 
-			long double ppsOffs = (ts_sec-ts.tv_sec)+ts_nsec*1e-9;
+            long double meanDiff=pigpioHandler->clockMeasurementOffset;
+
+            long double dx=timestamp-pigpioHandler->lastTimeMeasurementTick;
+            long double dy=pigpioHandler->clockMeasurementSlope*dx;
+            meanDiff+=dy;
+
+            qint64 meanDiffInt=(qint64)meanDiff;
+            double meanDiffFrac=(meanDiff-(qint64)meanDiff);
+            timestamp+=meanDiffInt; // add diff to real time
+            long int ts_sec=timestamp/1000000+(t0/1000);             // conv. us to s
+            long int ts_nsec=1000*(timestamp%1000000)+(t0 % 1000) * 1000000L;
+            ts_nsec+=(long int)(1000.*meanDiffFrac);
+
+            long double ppsOffs = (ts_sec-ts.tv_sec)+ts_nsec*1e-9;
 //			qDebug() << "PPS Offset: " << (double)(ppsOffs)*1e6 << " us";
             if (std::fabs(ppsOffs) < 3600.) {
                 qint32 t_diff_us = (double)(ppsOffs)*1e6;
@@ -192,12 +192,12 @@ static void cbFunction(int user_pi, unsigned int user_gpio,
         pigpioHandler = 0;
         pigpio_stop(pi);
         qCritical() << "Exception catched in 'static void cbFunction(int user_pi, unsigned int user_gpio, unsigned int level, uint32_t tick)':" << e.what();
-		qCritical() << "with user_pi="<<user_pi<<"user_gpio="<<user_gpio<<"level="<<level<<"tick="<<tick;
+        qCritical() << "with user_pi="<<user_pi<<"user_gpio="<<user_gpio<<"level="<<level<<"tick="<<tick;
     }
 }
 
 PigpiodHandler::PigpiodHandler(QVector<unsigned int> gpioPins, unsigned int spi_freq, uint32_t spi_flags, QObject *parent)
-	: QObject(parent)
+    : QObject(parent)
 {
     startOfProgram = QDateTime::currentDateTimeUtc();
     lastSamplingTime = startOfProgram;
@@ -210,22 +210,22 @@ PigpiodHandler::PigpiodHandler(QVector<unsigned int> gpioPins, unsigned int spi_
         //qDebug() << "could not start pigpio. Is pigpiod running?";
         //qDebug() << "you can start pigpiod with: sudo pigpiod -s 1";
         qFatal("Could not connect to pigpio daemon. Is pigpiod running? Start with sudo pigpiod -s 1");
-		return;
+        return;
     }
 
     isInitialised = true;
 
 //    gpioPins=gpio_pins;
-	for (auto& gpioPin : gpioPins) {
+    for (auto& gpioPin : gpioPins) {
         //if (GPIO_SIGNAL_MAP[gpioPin].direction!=DIR_IN) continue;
-		set_mode(pi, gpioPin, PI_INPUT);
+        set_mode(pi, gpioPin, PI_INPUT);
 //        if (gpioPin==ADC_READY) set_pull_up_down(pi, GPIO_PINMAP[gpioPin], PI_PUD_UP);
 
             //qDebug() << "set callback for pin " << gpio_pin;
             int result=callback(pi, gpioPin, RISING_EDGE, cbFunction);
-			if (result<0) {
-				qCritical()<<"error registering gpio callback for BCM pin"<<gpioPin;
-			}
+            if (result<0) {
+                qCritical()<<"error registering gpio callback for BCM pin"<<gpioPin;
+            }
 //        callback(pi, gpio_pin, FALLING_EDGE, cbFunction);
         //        if (value==pigif_bad_malloc||
         //            value==pigif_dublicate_callback||
@@ -262,11 +262,11 @@ void PigpiodHandler::setGpioState(unsigned int gpio, bool state) {
 }
 
 void PigpiodHandler::registerForCallback(unsigned int gpio, bool edge) {
-	int result=callback(pi, gpio, edge?FALLING_EDGE:RISING_EDGE, cbFunction);
-	if (result<0) {
-		GPIO_PIN pin=bcmToGpioSignal(gpio);
-		qCritical()<<"error registering gpio callback for BCM pin"<<GPIO_SIGNAL_MAP[pin].name;
-	}
+    int result=callback(pi, gpio, edge?FALLING_EDGE:RISING_EDGE, cbFunction);
+    if (result<0) {
+        GPIO_PIN pin=bcmToGpioSignal(gpio);
+        qCritical()<<"error registering gpio callback for BCM pin"<<GPIO_SIGNAL_MAP[pin].name;
+    }
 }
 
 void PigpiodHandler::writeSpi(uint8_t command, std::string data){
@@ -309,7 +309,7 @@ void PigpiodHandler::readSpi(uint8_t command, unsigned int bytesToRead){
         txBuf[i] = 0;
     }
     if (spi_xfer(pi, spiHandle, txBuf, rxBuf, bytesToRead+1)!=1+bytesToRead){
-		qWarning() << "readSpi(uint8_t, unsigned int): wrong number of bytes transfered";
+        qWarning() << "readSpi(uint8_t, unsigned int): wrong number of bytes transfered";
         return;
     }
 
@@ -393,41 +393,41 @@ void PigpiodHandler::measureGpioClockTime() {
     static qint64 diff_array[N];
     static quint64 tick_array[N];
     struct timespec tp, tp1, tp2;
-	
-	quint64 t0 = startOfProgram.toMSecsSinceEpoch();
-	
+
+    quint64 t0 = startOfProgram.toMSecsSinceEpoch();
+
     clock_gettime(CLOCK_REALTIME, &tp1);
-	uint32_t tick = get_current_tick(pi);
+    uint32_t tick = get_current_tick(pi);
     clock_gettime(CLOCK_REALTIME, &tp2);
 //        clock_gettime(CLOCK_MONOTONIC, &tp);
-	
-	qint64 dt = tp2.tv_sec-tp1.tv_sec;
-	dt *= 1000000000LL;
-	dt += (tp2.tv_nsec-tp1.tv_nsec);
-	dt /= 2000;
 
-	tp = tp1;
-	
-	if (tick<oldTick)
+    qint64 dt = tp2.tv_sec-tp1.tv_sec;
+    dt *= 1000000000LL;
+    dt += (tp2.tv_nsec-tp1.tv_nsec);
+    dt /= 2000;
+
+    tp = tp1;
+
+    if (tick<oldTick)
     {
-		gpioTickOverflowCounter = gpioTickOverflowCounter + UINT32_MAX + 1;
-	}
-	oldTick=tick;
-	quint64 nr_usecs = ((quint64)tp.tv_sec*1000-t0)*1000;
-	nr_usecs+=tp.tv_nsec/1000 + dt;
-	diff_array[arrayIndex]=(qint64)(nr_usecs-gpioTickOverflowCounter)-tick;
-	tick_array[arrayIndex]=(quint64)gpioTickOverflowCounter+tick;
-	lastTimeMeasurementTick = (quint64)gpioTickOverflowCounter+tick;
-	if (++arrayIndex>=N) {
-		arrayIndex=0;
-	}
-	if (nrSamples<N) nrSamples++;
-	 
-	double offs = 0.;
-	double slope = 0.;
-	calcLinearCoefficients(nrSamples, tick_array, diff_array, &offs, &slope, arrayIndex);
-	clockMeasurementSlope = slope;
-	clockMeasurementOffset = offs;
-	
+        gpioTickOverflowCounter = gpioTickOverflowCounter + UINT32_MAX + 1;
+    }
+    oldTick=tick;
+    quint64 nr_usecs = ((quint64)tp.tv_sec*1000-t0)*1000;
+    nr_usecs+=tp.tv_nsec/1000 + dt;
+    diff_array[arrayIndex]=(qint64)(nr_usecs-gpioTickOverflowCounter)-tick;
+    tick_array[arrayIndex]=(quint64)gpioTickOverflowCounter+tick;
+    lastTimeMeasurementTick = (quint64)gpioTickOverflowCounter+tick;
+    if (++arrayIndex>=N) {
+        arrayIndex=0;
+    }
+    if (nrSamples<N) nrSamples++;
+
+    double offs = 0.;
+    double slope = 0.;
+    calcLinearCoefficients(nrSamples, tick_array, diff_array, &offs, &slope, arrayIndex);
+    clockMeasurementSlope = slope;
+    clockMeasurementOffset = offs;
+
 //	qDebug() << "gpio clock measurement: N=" << nrSamples << " offs=" << offs << " slope=" << slope;
 }
