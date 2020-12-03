1cefc70b0671f4e9874a94824dc042a237d7db43
Daniel Treffenstädt <daniel@treffenstaedt.de>
2020-12-03 13:44:41 +0100

    Implemented TimeBaseSupervisor

--- source/cluster/include/timebasesupervisor.h
+++ source/cluster/include/timebasesupervisor.h
@@ -35,8 +35,8 @@ public:
 
 private:
     std::chrono::steady_clock::time_point m_start { std::chrono::steady_clock::now() };
-    std::chrono::steady_clock::time_point m_earliest {};
-    std::chrono::steady_clock::time_point m_latest {};
+    std::chrono::steady_clock::time_point m_earliest {std::chrono::steady_clock::now() + std::chrono::hours{24}};
+    std::chrono::steady_clock::time_point m_latest {std::chrono::steady_clock::now() - std::chrono::hours{24}};
     std::size_t m_n { 0 };
     std::chrono::steady_clock::duration m_current {};
 
--- source/cluster/src/timebasesupervisor.cpp
+++ source/cluster/src/timebasesupervisor.cpp
@@ -4,16 +4,25 @@
 namespace MuonPi {
 auto TimeBaseSupervisor::restart() -> std::chrono::steady_clock::duration
 {
-    return {};
+    m_current = m_latest - m_earliest;
+    m_latest = std::chrono::steady_clock::now() - std::chrono::hours{24};
+    m_earliest = std::chrono::steady_clock::now() + std::chrono::hours{24};
+    return m_current;
 }
 
-void TimeBaseSupervisor::process_event(const std::unique_ptr<Event>& /*event*/)
+void TimeBaseSupervisor::process_event(const std::unique_ptr<Event>& event)
 {
-
+    m_n++;
+    if (event->start() < m_earliest) {
+        m_earliest = event->start();
+    }
+    if (event->end() > m_latest) {
+        m_latest = event->start();
+    }
 }
 
 auto TimeBaseSupervisor::current() const -> std::chrono::steady_clock::duration
 {
-    return {};
+    return m_current;
 }
 }
