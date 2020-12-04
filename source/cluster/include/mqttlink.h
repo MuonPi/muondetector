d810775c37aefa178aa2b96ff6a3b22b58368965
Daniel Treffenstädt <daniel@treffenstaedt.de>
2020-12-04 17:36:05 +0100

    Added initial MqttLink implementation

--- source/cluster/include/mqttlink.h
+++ source/cluster/include/mqttlink.h
@@ -1,16 +1,85 @@
 #ifndef MQTTLINK_H
 #define MQTTLINK_H
 
+#include <string>
+#include <memory>
+#include <chrono>
+#include <map>
+#include <future>
+
+#include <mqtt/async_client.h>
+
 namespace MuonPi {
 
 /**
  * @brief The MqttLink class
  */
-class MqttLink
+class MqttLink : public mqtt::callback
 {
 public:
-    MqttLink();
-    ~MqttLink();
+    class Publisher {
+    public:
+        Publisher(mqtt::async_client& client, const std::string& topic);
+        [[nodiscard]] auto publish(const std::string& content) -> bool;
+    };
+
+    class Subscriber {
+    public:
+        Subscriber(mqtt::async_client& client, const std::string& topic);
+        [[nodiscard]] auto has_message() const -> bool;
+        [[nodiscard]] auto get_message() -> std::pair<std::string, std::chrono::steady_clock::time_point>;
+        void push_message(std::pair<std::string, std::chrono::steady_clock::time_point> message);
+    private:
+        std::queue<std::pair<std::string, std::chrono::steady_clock::time_point>> m_messages {};
+    };
+
+    struct LoginData
+    {
+        std::string username {};
+        std::string station_id {};
+        std::string client_id {};
+        std::string password {};
+    };
+
+    enum class Status {
+        Invalid,
+        Connected,
+        Disconnected,
+        Connecting,
+        Error
+    };
+
+    MqttLink(const std::string& server, const LoginData& login);
+
+    ~MqttLink() override;
+
+    [[nodiscard]] auto publish(const std::string& topic) -> std::shared_ptr<Publisher>;
+    [[nodiscard]] auto subscribe(const std::string& topic) -> std::shared_ptr<Subscriber>;
+
+    void connected(const std::string& cause) override;
+    void connection_lost(const std::string& cause) override;
+    void message_arrived(mqtt::const_message_ptr message) override;
+    void delivery_complete(mqtt::delivery_token_ptr token) override;
+
+private:
+    [[nodiscard]] auto connect() -> bool;
+    [[nodiscard]] auto disconnect() -> bool;
+    [[nodiscard]] auto reconnect() -> bool;
+
+    void set_status(Status status);
+
+    std::string m_server {};
+    LoginData m_login_data {};
+
+    Status m_status { Status::Invalid };
+
+    std::map<std::string, std::shared_ptr<Publisher>> m_publishers {};
+    std::map<std::string, std::shared_ptr<Subscriber>> m_subscribers {};
+
+    std::unique_ptr<mqtt::async_client> m_client { nullptr };
+    mqtt::connect_options m_conn_options {};
+
+    std::future<bool> m_connection_status {};
 };
 
 }
--- source/cluster/src/mqttlink.cpp
+++ source/cluster/src/mqttlink.cpp
@@ -1,9 +1,128 @@
 #include "mqttlink.h"
 
+#include <functional>
+#include <sstream>
+
 namespace MuonPi {
 
-MqttLink::MqttLink() = default;
+MqttLink::MqttLink(const std::string& server, const LoginData& login)
+    : m_server { server }
+    , m_login_data { login }
+    , m_client { std::make_unique<mqtt::async_client>(server, login.client_id) }
+{
+    m_conn_options.set_user_name(login.username);
+    m_conn_options.set_password(login.password);
+//    m_conn_options.set_keep_alive_interval(Config::MQTT::keepalive_interval);
 
-MqttLink::~MqttLink() = default;
+    m_connection_status = std::async(std::launch::async, &MqttLink::connect, this);
+}
 
+MqttLink::~MqttLink()
+{
+    if (!disconnect()) {
+    }
+}
+
+auto MqttLink::publish(const std::string& topic) -> std::shared_ptr<Publisher>
+{
+    if (m_status != Status::Connected) {
+        return nullptr;
+    }
+    if (m_publishers.find(topic) != m_publishers.end()) {
+        return nullptr;
+    }
+    m_publishers[topic] = std::make_shared<Publisher>(*m_client, topic);
+    return m_publishers[topic];
+}
+
+auto MqttLink::subscribe(const std::string& topic) -> std::shared_ptr<Subscriber>
+{
+    if (m_status != Status::Connected) {
+        return nullptr;
+    }
+    if (m_subscribers.find(topic) != m_subscribers.end()) {
+        return nullptr;
+    }
+    m_subscribers[topic] = std::make_shared<Subscriber>(*m_client, topic);
+    return m_subscribers[topic];
+}
+
+auto MqttLink::connect() -> bool
+{
+    set_status(Status::Connecting);
+    static constexpr std::size_t max_tries { 5 };
+    static std::size_t n { 0 };
+
+    if (n > max_tries) {
+        set_status(Status::Error);
+        return false;
+    }
+    try {
+        m_client->connect(m_conn_options)->wait();
+        set_status(Status::Connected);
+        m_client->set_callback(*this);
+        n = 0;
+        return true;
+    } catch (const mqtt::exception& /*exc*/) {
+        std::this_thread::sleep_for( std::chrono::seconds{1} );
+        n++;
+        return connect();
+    }
+}
+
+auto MqttLink::disconnect() -> bool
+{
+    if (m_status != Status::Connected) {
+        return true;
+    }
+    try {
+        m_client->disconnect()->wait();
+        set_status(Status::Disconnected);
+        return true;
+    } catch (const mqtt::exception& /*exc*/) {
+        return false;
+    }
+}
+
+auto MqttLink::reconnect() -> bool
+{
+    set_status(Status::Disconnected);
+    try {
+        if (!m_client->reconnect()->wait_for(std::chrono::seconds{5})) {
+            return false;
+        }
+        set_status(Status::Connected);
+        return true;
+    } catch (const mqtt::exception& /*exc*/) {
+        return false;
+    }
+}
+
+void MqttLink::set_status(Status status) {
+    m_status = status;
+}
+
+void MqttLink::connected(const std::string& /*cause*/)
+{
+
+}
+
+void MqttLink::connection_lost(const std::string& /*cause*/)
+{
+
+}
+
+void MqttLink::message_arrived(mqtt::const_message_ptr message)
+{
+    auto now { std::chrono::steady_clock::now() };
+    auto subscriber { m_subscribers.find(message->get_topic()) };
+    if (subscriber == m_subscribers.end()) {
+        return;
+    }
+    (*subscriber).second->push_message(std::make_pair(message->to_string(), now));
+}
+
+void MqttLink::delivery_complete(mqtt::delivery_token_ptr /*token*/)
+{
+}
 }
