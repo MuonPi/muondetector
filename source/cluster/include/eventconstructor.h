70327ff79d0581b433c1d9bb2dae40e4036bc6db
Daniel Treffenstädt <daniel@treffenstaedt.de>
2020-12-04 14:13:54 +0100

    Implemented Event construction

--- source/cluster/include/eventconstructor.h
+++ source/cluster/include/eventconstructor.h
@@ -16,41 +16,59 @@ class Criterion;
 class EventConstructor
 {
 public:
-    /**
-     * @brief EventConstructor
-     * @param event
-     * @param criterion
-     */
-    EventConstructor(std::unique_ptr<Event> event, std::shared_ptr<Criterion> criterion);
-    ~EventConstructor();
-    /**
-     * @brief add_event
-     * @param event
-     */
-    auto add_event(std::unique_ptr<Event> event) -> bool;
+    enum class Type {
+        NoMatch,
+        Contested,
+        Match
+    };
 
     /**
-     * @brief set_timeout
-     * @param timeout
+     * @brief EventConstructor
+     * @param event The initial event from which the constructor stems
+     * @param criterion The criterion to use to check events
+     * @param timeout the initial timeout to use
+     */
+    EventConstructor(std::unique_ptr<Event> event, std::shared_ptr<Criterion> criterion, std::chrono::steady_clock::duration timeout);
+
+    ~EventConstructor();
+
+    /**
+     * @brief add_event
+     * @param event The event to add
+     * @param contested Whether the event is contested or not
+     */
+    void add_event(std::unique_ptr<Event> event, bool contested = false);
+
+    /**
+     * @brief event_matches Check whether an event matches the criterion
+     * @param event The event to check
+     * @return The type of match
+     */
+    [[nodiscard]] auto event_matches(const Event& event) -> Type;
+
+    /**
+     * @brief set_timeout Set a new timeout for the EventConstructor. Only accepts longer timeouts.
+     * @param timeout The timeout to set.
      */
     void set_timeout(std::chrono::steady_clock::duration timeout);
 
     /**
-     * @brief commit
-     * @return
+     * @brief commit Hand over the event unique_ptr
+     * @return The unique_ptr to the constructed event
      */
     [[nodiscard]] auto commit() -> std::unique_ptr<Event>;
+
     /**
-     * @brief timed_out
-     * @return
+     * @brief timed_out Check whether the timeout has been reached
+     * @return true if the constructor timed out.
      */
     [[nodiscard]] auto timed_out() const -> bool;
 
 private:
-    std::chrono::steady_clock::duration m_timeout { std::chrono::minutes{1} };
     std::chrono::steady_clock::time_point m_start { std::chrono::steady_clock::now() };
     std::unique_ptr<CombinedEvent> m_event { nullptr };
     std::shared_ptr<Criterion> m_criterion { nullptr };
+    std::chrono::steady_clock::duration m_timeout { std::chrono::minutes{1} };
 };
 
 }
--- source/cluster/src/eventconstructor.cpp
+++ source/cluster/src/eventconstructor.cpp
@@ -5,33 +5,44 @@
 
 namespace MuonPi {
 
-EventConstructor::EventConstructor(std::unique_ptr<Event> event, std::shared_ptr<Criterion> criterion)
+EventConstructor::EventConstructor(std::unique_ptr<Event> event, std::shared_ptr<Criterion> criterion, std::chrono::steady_clock::duration timeout)
     : m_event { std::make_unique<CombinedEvent>(event->id(), std::move(event)) }
     , m_criterion { criterion }
+    , m_timeout { timeout }
 {
 }
 
 EventConstructor::~EventConstructor() = default;
 
-auto EventConstructor::add_event(std::unique_ptr<Event> event) -> bool
+void EventConstructor::add_event(std::unique_ptr<Event> event, bool contested)
 {
-
-    const float criterion { m_criterion->criterion(*m_event, *event) };
-
-    if (criterion < m_criterion->maximum_false()) {
-        return false;
-    }
-
     m_event->add_event(std::move(event));
 
-    if (criterion < m_criterion->minimum_true()) {
+    if (contested) {
         m_event->mark_contested();
     }
-    return true;
+}
+
+auto EventConstructor::event_matches(const Event& event) -> EventConstructor::Type
+{
+    const float criterion { m_criterion->criterion(*m_event, event) };
+
+    if (criterion < m_criterion->maximum_false()) {
+        return Type::NoMatch;
+    }
+
+    if (criterion < m_criterion->minimum_true()) {
+        return Type::Contested;
+    }
+
+    return Type::Match;
 }
 
 void EventConstructor::set_timeout(std::chrono::steady_clock::duration timeout)
 {
+    if (timeout <= m_timeout) {
+        return;
+    }
     m_timeout = timeout;
 }
 
