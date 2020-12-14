ea1c76a2622ede306469b4b49035b749179f4963
Daniel Treffenstädt <daniel@treffenstaedt.de>
2020-12-14 11:42:34 +0100

    Added implementation for log parser

--- source/cluster/include/mqttlogsource.h
+++ source/cluster/include/mqttlogsource.h
@@ -5,10 +5,40 @@
 #include "mqttlink.h"
 
 #include <memory>
+#include <map>
 
 namespace MuonPi {
 
 class LogMessage;
+class MessageParser;
+
+struct LogItem {
+    static constexpr std::uint8_t s_default_status { 0xFF };
+    std::string id {};
+    std::uint8_t status { s_default_status };
+
+    struct {
+        double h;
+        double lat;
+        double lon;
+        double h_acc;
+        double v_acc;
+        double dop;
+    } geo;
+
+    struct {
+        double accuracy;
+        double dop;
+    } time;
+
+    void reset();
+
+    [[nodiscard]] auto add(MessageParser& message) -> bool;
+
+    [[nodiscard]] auto complete() -> bool;
+
+};
+
 
 /**
  * @brief The MqttLogSource class
@@ -35,6 +65,10 @@ protected:
 
 private:
     std::shared_ptr<MqttLink::Subscriber> m_link { nullptr };
+
+    void process(LogItem item);
+
+    std::map<std::size_t, LogItem> m_buffer {};
 };
 
 }
--- source/cluster/src/mqttlogsource.cpp
+++ source/cluster/src/mqttlogsource.cpp
@@ -2,8 +2,57 @@
 
 #include "logmessage.h"
 #include "utility.h"
+#include "log.h"
 
 namespace MuonPi {
+
+void LogItem::reset()
+{
+    id = "";
+    status = s_default_status;
+}
+
+auto LogItem::add(MessageParser& message) -> bool
+{
+    if (id != message[0]) {
+        Log::debug()<<"Resetting log aggregation.";
+        reset();
+        id = message[0];
+    }
+    if (message[1] == "geoHeightMSL") {
+        geo.h = std::stod(message[2], nullptr);
+        status &= ~1;
+    } else if (message[1] == "geoHorAccuracy") {
+        geo.h_acc = std::stod(message[2], nullptr);
+        status &= ~2;
+    } else if (message[1] == "geoLatitude") {
+        geo.lat = std::stod(message[2], nullptr);
+        status &= ~4;
+    } else if (message[1] == "geoLongitude") {
+        geo.lon = std::stod(message[2], nullptr);
+        status &= ~8;
+    } else if (message[1] == "geoVertAccuracy") {
+        geo.v_acc = std::stod(message[2], nullptr);
+        status &= ~16;
+    } else if (message[1] == "positionDOP") {
+        geo.dop = std::stod(message[2], nullptr);
+        status &= ~32;
+    } else if (message[1] == "timeAccuracy") {
+        time.accuracy = std::stod(message[2], nullptr);
+        status &= ~64;
+    } else if (message[1] == "timeDOP") {
+        time.dop = std::stod(message[2], nullptr);
+        status &= ~128;
+    } else {
+        return false;
+    }
+    return true;
+}
+
+auto LogItem::complete() -> bool
+{
+    return !status;
+}
 
 MqttLogSource::MqttLogSource(std::shared_ptr<MqttLink::Subscriber> subscriber)
     : m_link { subscriber }
@@ -27,11 +76,35 @@ auto MqttLogSource::step() -> int
         MqttLink::Message msg = m_link->get_message();
         MessageParser topic { msg.topic, '/'};
         MessageParser content { msg.content, ' '};
-        // todo: parsing of message
-//        push_item(nullptr);
+        if ((topic.size() == 4) && (content.size() >= 2)) {
+            std::size_t hash {std::hash<std::string>{}(topic[2] + topic[3])};
+
+            if (m_buffer.find(hash) != m_buffer.end()) {
+                auto& item { m_buffer[hash] };
+                if (item.add(content)) {
+                    Log::info()<<"Got log from " + topic[2] + topic[3] + ": " + content[1];
+                }
+                if (item.complete()) {
+                    Log::info()<<"Got last log from " + topic[2] + topic[3];
+                    process(item);
+                    m_buffer.erase(hash);
+                }
+            } else {
+                LogItem item;
+                item.id = content[0];
+                if (item.add(content)) {
+                    Log::info()<<"Got first log from " + topic[2] + topic[3];
+                    Log::info()<<"Got log from " + topic[2] + topic[3] + ": " + content[1];
+                }
+                m_buffer[hash] = item;
+            }
+        }
     }
     std::this_thread::sleep_for(std::chrono::microseconds{50});
     return 0;
 }
 
+void MqttLogSource::process(LogItem /*item*/)
+{
+}
 }
