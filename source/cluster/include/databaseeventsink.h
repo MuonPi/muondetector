cd2234e2fdc8291c84e16d2696d9e4356e172b78
hangeza <zaunick@exp2.physik.uni-giessen.de>
2020-12-17 16:58:10 +0100

    implemented DatabaseSink::step(), still not finished

--- source/cluster/include/databaseeventsink.h
+++ source/cluster/include/databaseeventsink.h
@@ -17,13 +17,20 @@ class DatabaseEventSink : public AbstractSink<Event>
 {
 protected:
     /**
+     * @brief DatabaseEventSink
+     * @param The 
+     */
+    DatabaseEventSink();
+
+	
+	/**
      * @brief step implementation from ThreadRunner
      * @return zero if the step succeeded.
      */
     [[nodiscard]] auto step() -> int override;
 
 private:
-	void process(Event evt);
+	void process(Event event);
 	std::unique_ptr<DatabaseLink> m_link { nullptr };
 };
 
--- source/cluster/src/databaseeventsink.cpp
+++ source/cluster/src/databaseeventsink.cpp
@@ -14,12 +14,29 @@ auto DatabaseEventSink::step() -> int
     return 0;
 }
 
-void DatabaseEventSink::process(Event evt)
+void DatabaseEventSink::process(Event event)
 {
-    DbEntry entry { };
-	entry.measurement()="L1Event";
-	entry.timestamp()=std::to_string(evt.start());
-
+    if (event.n() == 1) {
+		// by default, don't write the single events to the db
+		return;
+	}
+	
+	std::vector<Event> events = event.events();
+	auto evt_it = events.begin();
+	for (;evt_it != events.end(); evt_it++) {
+		DbEntry entry { };
+		// measurement name
+		entry.measurement()="L1Event";
+		// timestamp
+		entry.timestamp()=std::to_string(evt_it->start());
+		// tags
+		entry.tags().push_back(std::make_pair("user", evt_it->data().user));
+		entry.tags().push_back(std::make_pair("detector", evt_it->data().station_id));
+		entry.tags().push_back(std::make_pair("site_id", evt_it->data().user+evt_it->data().station_id));
+		// fields
+		entry.fields().push_back(std::make_pair("accuracy", std::to_string(evt_it->data().time_acc)));
+		if (m_link->write_entry(entry)) return;
+	}
     // todo: construct message string
 }
  
