3c1437f7a00e171a3502dbd3a06534eb9d958ec5
Daniel Treffenstädt <daniel@treffenstaedt.de>
2020-12-21 12:06:59 +0100

    Changed code to use libcurl instead of libcurlpp

--- source/cluster/src/databaselink.cpp
+++ source/cluster/src/databaselink.cpp
@@ -1,15 +1,119 @@
-#include "databaselink.h"
+﻿#include "databaselink.h"
 
 #include "log.h"
 
 #include <type_traits>
 #include <variant>
 
-#include <curlpp/cURLpp.hpp>
-#include <curlpp/Easy.hpp>
-#include <curlpp/Options.hpp>
-#include <curlpp/Exception.hpp>
+#include <curl/curl.h>
 
+////////////////////////////////////////////////////////////////////////////////
+/// ++++++++++++++++++
+/// only for debugging
+/// ++++++++++++++++++
+////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+struct data {
+  char trace_ascii; /* 1 or 0 */
+};
+
+static
+void dump(const char *text,
+          FILE *stream, unsigned char *ptr, size_t size,
+          char nohex)
+{
+  size_t i;
+  size_t c;
+
+  unsigned int width = 0x10;
+
+  if(nohex)
+    /* without the hex output, we can fit more on screen */
+    width = 0x40;
+
+  fprintf(stream, "%s, %10.10lu bytes (0x%8.8lx)\n",
+          text, (unsigned long)size, (unsigned long)size);
+
+  for(i = 0; i<size; i += width) {
+
+    fprintf(stream, "%4.4lx: ", (unsigned long)i);
+
+    if(!nohex) {
+      /* hex not disabled, show it */
+      for(c = 0; c < width; c++)
+        if(i + c < size)
+          fprintf(stream, "%02x ", ptr[i + c]);
+        else
+          fputs("   ", stream);
+    }
+
+    for(c = 0; (c < width) && (i + c < size); c++) {
+      /* check for 0D0A; if found, skip past and start a new line of output */
+      if(nohex && (i + c + 1 < size) && ptr[i + c] == 0x0D &&
+         ptr[i + c + 1] == 0x0A) {
+        i += (c + 2 - width);
+        break;
+      }
+      fprintf(stream, "%c",
+              (ptr[i + c] >= 0x20) && (ptr[i + c]<0x80)?ptr[i + c]:'.');
+      /* check again for 0D0A, to avoid an extra \n if it's at width */
+      if(nohex && (i + c + 2 < size) && ptr[i + c + 1] == 0x0D &&
+         ptr[i + c + 2] == 0x0A) {
+        i += (c + 3 - width);
+        break;
+      }
+    }
+    fputc('\n', stream); /* newline */
+  }
+  fflush(stream);
+}
+
+static
+int my_trace(CURL *handle, curl_infotype type,
+             char *data, size_t size,
+             void *userp)
+{
+  struct data *config = (struct data *)userp;
+  const char *text;
+  (void)handle; /* prevent compiler warning */
+
+  switch(type) {
+  case CURLINFO_TEXT:
+    fprintf(stderr, "== Info: %s", data);
+    /* FALLTHROUGH */
+  default: /* in case a new one is introduced to shock us */
+    return 0;
+
+  case CURLINFO_HEADER_OUT:
+    text = "=> Send header";
+    break;
+  case CURLINFO_DATA_OUT:
+    text = "=> Send data";
+    break;
+  case CURLINFO_SSL_DATA_OUT:
+    text = "=> Send SSL data";
+    break;
+  case CURLINFO_HEADER_IN:
+    text = "<= Recv header";
+    break;
+  case CURLINFO_DATA_IN:
+    text = "<= Recv data";
+    break;
+  case CURLINFO_SSL_DATA_IN:
+    text = "<= Recv SSL data";
+    break;
+  }
+
+  dump(text, stderr, (unsigned char *)data, size, config->trace_ascii);
+  return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// ------------------
+/// only for debugging
+/// ------------------
+////////////////////////////////////////////////////////////////////////////////
 
 namespace MuonPi {
 DatabaseLink::Entry::Entry(const std::string& measurement, DatabaseLink& link)
@@ -67,38 +171,46 @@ auto DatabaseLink::measurement(const std::string& measurement) -> Entry
 auto DatabaseLink::send_string(const std::string& query) -> bool
 {
 
-    try {
-      curlpp::Cleanup cleaner;
-      curlpp::Easy request;
+    CURL *curl;
+    struct data config;
+    std::ostringstream url {};
+    url
+        <<m_server
+        <<"/write?db="
+        <<m_database
+        <<"&u="<<m_login_data.username
+        <<"&p="<<m_login_data.password
+        <<"&epoch=ms";
 
+    config.trace_ascii = 1; /* enable ascii tracing */
 
-      std::ostringstream url {};
-      url
-            <<m_server
-            <<" /write?db="
-            <<m_database
-            <<"&u="<<m_login_data.username
-            <<"&p="<<m_login_data.password
-            <<"&epoch=ms";
+    curl = curl_easy_init();
+    if(curl) {
+        // +++ only for debugging
+        curl_easy_setopt(curl, CURLOPT_DEBUGFUNCTION, my_trace);
+        curl_easy_setopt(curl, CURLOPT_DEBUGDATA, &config);
+        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+        // --- only for debugging
 
-      request.setOpt(new curlpp::options::Url(url.str()));
-      request.setOpt(new curlpp::options::Port(s_port));
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
-      request.setOpt(new curlpp::options::PostFields(query));
-      request.setOpt(new curlpp::options::PostFieldSize(static_cast<long>(query.length())));
-      // +++ Debugging only
-      request.setOpt(new curlpp::options::Verbose(true));
-      request.setOpt((new curlpp::options::WriteStream(&std::cerr)));
-      // --- Debugging only
-      request.perform();
+        curl_easy_setopt(curl, CURLOPT_URL, url.str().c_str());
+
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, query.c_str());
+
+        CURLcode res { curl_easy_perform(curl) };
+
+        long http_code { 0 };
+        curl_easy_getinfo (curl, CURLINFO_RESPONSE_CODE, &http_code);
+
+        if((res != CURLE_OK)) {
+            Log::warning()<<"Couldn't query Database: " + std::string{curl_easy_strerror(res)};
+        }
+
+        /* always cleanup */
+        curl_easy_cleanup(curl);
     }
-    catch ( curlpp::LogicError & e ) {
-      std::cout << e.what() << std::endl;
-    }
-    catch ( curlpp::RuntimeError & e ) {
-      std::cout << e.what() << std::endl;
-    }
-    return {};
+    return true;
 }
 
 } // namespace MuonPi
