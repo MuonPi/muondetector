62041d7db79d370ef2a210c1888d71c8f79cf0f5
Daniel Treffenstädt <daniel@treffenstaedt.de>
2020-12-23 12:30:09 +0100

    Removed unnecessary Constructor, added virtual destructors, made function pure virtual

--- source/cluster/include/mqttlogsource.h
+++ source/cluster/include/mqttlogsource.h
@@ -20,11 +20,11 @@ namespace MuonPi {
 class AbstractMqttItemCollector {
 public:
     UserInfo user_info {};
-	std::string message_id {};
+    std::string message_id {};
 
-	AbstractMqttItemCollector() {}
-	
-	/**
+    virtual ~AbstractMqttItemCollector();
+
+    /**
      * @brief reset Resets the ItemCollector to its default state
      */
     void reset();
@@ -34,19 +34,20 @@ public:
      * @param message The message to pass
      * @return true if the Message was accepted. False in an error or if the message was not accepted
      */
-    virtual auto add(MessageParser& /*message*/) -> bool /*{return false;} */;
+    virtual auto add(MessageParser& message) -> bool = 0;
 
     /**
      * @brief complete indicates whether the Item has collected all required messages
      * @return true if it is complete
      */
     [[nodiscard]] auto complete() -> bool;
-	
+
 protected:
-	std::uint16_t m_status { 0 };
-	std::uint16_t default_status { 0x00FF };
+    std::uint16_t m_status { 0 };
+    std::uint16_t default_status { 0x00FF };
 };
 
+AbstractMqttItemCollector::~AbstractMqttItemCollector() = default;
 
 /**
  * @brief Helper class for complete collection of several logically connected DetectorLogItems
@@ -54,7 +55,7 @@ protected:
 class DetectorInfoCollector : public AbstractMqttItemCollector {
 public:
     //UserInfo user_info {};
-	//std::string message_id {};
+    //std::string message_id {};
     //std::uint8_t status { s_default_status };
 
     struct {
@@ -70,8 +71,10 @@ public:
         double accuracy;
         double dop;
     } time;
-	
-	    /**
+
+    virtual ~DetectorInfoCollector() override = default;
+
+        /**
      * @brief add Tries to add a Message to the Item. The item chooses which messages to keep
      * @param message The message to pass
      * @return true if the Message was accepted. False in an error or if the message was not accepted
@@ -130,8 +133,8 @@ private:
 // +++++++++++++++++++++++++++++++
 
 void AbstractMqttItemCollector::reset() {
-	user_info = UserInfo { };
-	m_status = default_status;
+    user_info = UserInfo { };
+    m_status = default_status;
 }
 
 auto AbstractMqttItemCollector::complete() -> bool
@@ -204,7 +207,7 @@ auto MqttLogSource<T>::step() -> int
 {
     if (m_link.has_message()) {
         MqttLink::Message msg = m_link.get_message();
-		this->process(msg);
+        this->process(msg);
     }
     std::this_thread::sleep_for(std::chrono::microseconds{50});
     return 0;
@@ -213,7 +216,7 @@ auto MqttLogSource<T>::step() -> int
 template <>
 void MqttLogSource<MqttLink::Message>::process(const MqttLink::Message& msg)
 {
-	push_item( MqttLink::Message { msg } );
+    push_item( MqttLink::Message { msg } );
 }
 
 template <>
@@ -222,13 +225,13 @@ void MqttLogSource<DetectorInfo>::process(const MqttLink::Message& msg)
         MessageParser topic { msg.topic, '/'};
         MessageParser content { msg.content, ' '};
         MessageParser subscribe_topic { m_link.get_subscribe_topic(), '/' };
-		
-		if ( topic[2] == "" || topic[2] == "cluster"  ) return;
-		
-		if ((topic.size() >= 4) && (content.size() >= 2)) {
+
+        if ( topic[2] == "" || topic[2] == "cluster"  ) return;
+
+        if ((topic.size() >= 4) && (content.size() >= 2)) {
             UserInfo userinfo {};
-			userinfo.username =  topic[2];
-			std::string site { topic[3] };
+            userinfo.username =  topic[2];
+            std::string site { topic[3] };
             for (std::size_t i = 4; i < topic.size(); i++) {
                 site += "/" + topic[i];
             }
@@ -239,25 +242,25 @@ void MqttLogSource<DetectorInfo>::process(const MqttLink::Message& msg)
                 DetectorInfoCollector& item = dynamic_cast<DetectorInfoCollector&>( m_buffer[hash] );
                 item.add(content);
 
-				if (item.complete()) {
-					static constexpr struct {
-						const double pos_dop { 1.0e-1 };
-						const double time_dop { 1.0e-1 };
-						const double v_accuracy { 1.0e-1 };
-						const double h_accuracy { 1.0e-1 };
-					} factors; // norming factors for the four values
+                if (item.complete()) {
+                    static constexpr struct {
+                        const double pos_dop { 1.0e-1 };
+                        const double time_dop { 1.0e-1 };
+                        const double v_accuracy { 1.0e-1 };
+                        const double h_accuracy { 1.0e-1 };
+                    } factors; // norming factors for the four values
 
-					Location location;
+                    Location location;
 
-					location.dop = (factors.pos_dop * item.geo.dop) * (factors.time_dop * item.time.dop);
-					location.prec = (factors.h_accuracy * item.geo.h_acc) * (factors.v_accuracy * item.geo.v_acc);
+                    location.dop = (factors.pos_dop * item.geo.dop) * (factors.time_dop * item.time.dop);
+                    location.prec = (factors.h_accuracy * item.geo.h_acc) * (factors.v_accuracy * item.geo.v_acc);
 
-					location.h = item.geo.h;
-					location.lat = item.geo.lat;
-					location.lon = item.geo.lon;
+                    location.h = item.geo.h;
+                    location.lat = item.geo.lat;
+                    location.lon = item.geo.lon;
 
-					this->push_item( DetectorInfo{hash, item.user_info, location} );
-					//process(hash, item);
+                    this->push_item( DetectorInfo{hash, item.user_info, location} );
+                    //process(hash, item);
                     m_buffer.erase(hash);
                 }
             } else {
