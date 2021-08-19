fa9a132dc105bfb4b6717456b221386c25d8de88
hangeza <zaunick@exp2.physik.uni-giessen.de>
2021-08-19 14:45:18 +0000

    re-introduced the code fragments which were unintentionally deleted in last commit

--- source/daemon/src/daemon.cpp
+++ source/daemon/src/daemon.cpp
@@ -217,6 +217,10 @@ Daemon::Daemon(configuration cfg, QObject* parent)
         qInfo() << this->thread()->objectName() << " thread id (pid): " << syscall(SYS_gettid);
     }
 
+    // connect logParameter signal to log engine before anything else is done
+    // since we want to log some initial one-time log parameters on start-up
+    connect(this, &Daemon::logParameter, &logEngine, &LogEngine::onLogParameterReceived);
+
     // try to find out on which hardware version we are running
     // for this to work, we have to initialize and read the eeprom first
     // EEPROM 24AA02 type
@@ -243,9 +247,10 @@ Daemon::Daemon(configuration cfg, QObject* parent)
             }
         }
         uint64_t id = calib->getSerialID();
-        QString hwIdStr = "0x" + QString::number(id, 16);
+        QString hwIdStr = QString::number(id, 16);
+        logParameter(LogParameter("uniqueId", hwIdStr, LogParameter::LOG_ONCE));
+		hwIdStr = "0x"+hwIdStr;
         qInfo() << "EEP unique ID:" << hwIdStr;
-
     } else {
         qCritical() << "eeprom device NOT present!";
     }
@@ -312,7 +317,6 @@ Daemon::Daemon(configuration cfg, QObject* parent)
     fileHandlerThread->start();
 
     // connect log signals to and from log engine and filehandler
-    connect(this, &Daemon::logParameter, &logEngine, &LogEngine::onLogParameterReceived);
     connect(&logEngine, &LogEngine::sendLogString, mqttHandler, &MuonPi::MqttHandler::sendLog);
     connect(&logEngine, &LogEngine::sendLogString, fileHandler, &FileHandler::writeToLogFile);
     // connect to the regular log timer signal to log several non-regularly polled parameters
@@ -2147,9 +2151,9 @@ void Daemon::UBXReceivedVersion(const QString& swString, const QString& hwString
     TcpMessage tcpMessage(TCP_MSG_KEY::MSG_UBX_VERSION);
     (*tcpMessage.dStream) << swString << hwString << protString;
     emit sendTcpMessage(tcpMessage);
-    logParameter(LogParameter("UBX_SW_Version", swString, LogParameter::LOG_ONCE));
-    logParameter(LogParameter("UBX_HW_Version", hwString, LogParameter::LOG_ONCE));
-    logParameter(LogParameter("UBX_Prot_Version", protString, LogParameter::LOG_ONCE));
+    emit logParameter(LogParameter("UBX_SW_Version", swString, LogParameter::LOG_ONCE));
+    emit logParameter(LogParameter("UBX_HW_Version", hwString, LogParameter::LOG_ONCE));
+    emit logParameter(LogParameter("UBX_Prot_Version", protString, LogParameter::LOG_ONCE));
     if (initialVersionInfo) {
         configGpsForVersion();
         qInfo() << "Ublox version: " << hwString << " (fw:" << swString << "prot:" << protString << ")";
