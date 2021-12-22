39c09af07066d06b51c309d1c92eb58d24a93354
hangeza <zaunick@exp2.physik.uni-giessen.de>
2021-12-22 21:59:22 +0100

    moved Property class to muondetector_structs.h

--- daemon/include/daemon.h
+++ daemon/include/daemon.h
@@ -33,74 +33,9 @@
 
 struct CalibStruct;
 struct UbxTimeMarkStruct;
+class Property;
 enum GPIO_PIN;
 
-class Property {
-public:
-    Property() = default;
-
-    template <typename T>
-    Property(const T& val)
-        : name("")
-        , unit("")
-    {
-        typeId = qMetaTypeId<T>();
-        if (typeId == QMetaType::UnknownType) {
-            typeId = qRegisterMetaType<T>();
-        }
-        value = QVariant(val);
-        updated = true;
-    }
-
-    template <typename T>
-    Property(const QString& a_name, const T& val, const QString& a_unit = "")
-        : name(a_name)
-        , unit(a_unit)
-    {
-        typeId = qMetaTypeId<T>();
-        if (typeId == QMetaType::UnknownType) {
-            typeId = qRegisterMetaType<T>();
-        }
-        value = QVariant(val);
-        updated = true;
-    }
-
-    Property(const Property& prop) = default;
-    Property& operator=(const Property& prop)
-    {
-        name = prop.name;
-        unit = prop.unit;
-        value = prop.value;
-        typeId = prop.typeId;
-        updated = prop.updated;
-        return *this;
-    }
-
-    Property& operator=(const QVariant& val)
-    {
-        value = val;
-        //lastUpdate = std::chrono::system_clock::now();
-        updated = true;
-        return *this;
-    }
-
-    const QVariant& operator()()
-    {
-        updated = false;
-        return value;
-    }
-
-    bool isUpdated() const { return updated; }
-    int type() const { return typeId; }
-
-    QString name = "";
-    QString unit = "";
-
-private:
-    QVariant value;
-    bool updated = false;
-    int typeId = 0;
-};
 
 struct RateScanInfo {
     uint8_t origPcaMask = 0;
--- library/include/muondetector_structs.h
+++ library/include/muondetector_structs.h
@@ -8,6 +8,7 @@
 #include <QList>
 #include <QMap>
 #include <QString>
+#include <QVariant>
 #include <iomanip>
 #include <iostream>
 #include <string>
@@ -425,5 +426,73 @@ inline QDataStream& operator<<(QDataStream& out, const UbxTimeMarkStruct& tm)
         << tm.timeBase << tm.utcAvailable << tm.flags << tm.evtCounter;
     return out;
 }
+
+class Property {
+public:
+    Property() = default;
+
+    template <typename T>
+    Property(const T& val)
+        : name("")
+        , unit("")
+    {
+        typeId = qMetaTypeId<T>();
+        if (typeId == QMetaType::UnknownType) {
+            typeId = qRegisterMetaType<T>();
+        }
+        value = QVariant(val);
+        updated = true;
+    }
+
+    template <typename T>
+    Property(const QString& a_name, const T& val, const QString& a_unit = "")
+        : name(a_name)
+        , unit(a_unit)
+    {
+        typeId = qMetaTypeId<T>();
+        if (typeId == QMetaType::UnknownType) {
+            typeId = qRegisterMetaType<T>();
+        }
+        value = QVariant(val);
+        updated = true;
+    }
+
+    Property(const Property& prop) = default;
+    Property& operator=(const Property& prop)
+    {
+        name = prop.name;
+        unit = prop.unit;
+        value = prop.value;
+        typeId = prop.typeId;
+        updated = prop.updated;
+        return *this;
+    }
+
+    Property& operator=(const QVariant& val)
+    {
+        value = val;
+        //lastUpdate = std::chrono::system_clock::now();
+        updated = true;
+        return *this;
+    }
+
+    const QVariant& operator()()
+    {
+        updated = false;
+        return value;
+    }
+
+    bool isUpdated() const { return updated; }
+    int type() const { return typeId; }
+
+    QString name = "";
+    QString unit = "";
+
+private:
+    QVariant value;
+    bool updated = false;
+    int typeId = 0;
+};
+
 
 #endif // MUONDETECTOR_STRUCTS_H
