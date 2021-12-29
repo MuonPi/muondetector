68cfb9a871cbcc5ee36375d266e56392640dde75
hangeza <zaunick@exp2.physik.uni-giessen.de>
2021-12-29 13:59:21 +0100

    improved MCP4728 device class

--- daemon/include/hardware/i2c/mcp4728.h
+++ daemon/include/hardware/i2c/mcp4728.h
@@ -1,10 +1,12 @@
 #ifndef _MCP4728_H_
 #define _MCP4728_H_
 #include "hardware/i2c/i2cdevice.h"
+#include "hardware/device_types.h"
+#include <chrono>
 
 /* MCP4728 4ch 12bit DAC */
 
-class MCP4728 : public i2cDevice, public static_device_base<MCP4728> {
+class MCP4728 : public i2cDevice, public DeviceFunction<DeviceType::DAC>, public static_device_base<MCP4728> {
     // the DAC supports writing to input register but not sending latch bit to update the output register
     // here we will always send the "UDAC" (latch) bit because we don't need this functionality
     // MCP4728 listens to I2C Generall Call Commands
@@ -12,8 +14,6 @@ class MCP4728 : public i2cDevice, public static_device_base<MCP4728> {
     // reset is "0x00 0x06"
     // wake-up is "0x00 0x09"
 public:
-    // to ground to power down the circuit
-    const static float VDD; // change, if device powered with different voltage
     enum CFG_GAIN { GAIN1 = 0,
         GAIN2 = 1 };
     enum CFG_VREF { VREF_VDD = 0,
@@ -46,10 +46,11 @@ public:
     }
     bool devicePresent();
     bool setVoltage(uint8_t channel, float voltage, bool toEEPROM = false);
-    bool setValue(uint8_t channel, uint16_t value, uint8_t gain = GAIN1, bool toEEPROM = false);
+    bool setValue(uint8_t channel, uint16_t value, CFG_GAIN gain = GAIN1, bool toEEPROM = false);
     bool writeChannel(uint8_t channel, const DacChannel& channelData);
     bool readChannel(uint8_t channel, DacChannel& channelData);
-
+	bool setVRef( unsigned int channel, CFG_VREF vref_setting );
+	
     static float code2voltage(const DacChannel& channelData);
 
 	bool identify() override;
@@ -58,6 +59,22 @@ public:
 
 private:
     unsigned int fLastConvTime;
+	static constexpr float fVddRefVoltage { 3.3 }; ///< voltage at which the device is powered
+	enum COMMAND: uint8_t {
+		DAC_FAST_WRITE		= 	0b00000000,
+		DAC_MULTI_WRITE		=	0b00001000,
+		DAC_EEP_SEQ_WRITE	=	0b00001010,
+		DAC_EEP_SINGLE_WRITE=	0b00001011,
+		ADDR_BITS_WRITE		=	0b00001100,
+		VREF_WRITE			= 	0b00010000,
+		GAIN_WRITE			=	0b00011000,
+		PD_WRITE			=	0b00010100
+	};
+	DacChannel fChannelSetting[4], fChannelSettingEep[4];
+	std::chrono::time_point<std::chrono::steady_clock> fLastRegisterUpdate { };
+	
+	bool readRegisters();
+	void parseChannelData( uint8_t* buf );
 };
 
 #endif //!_MCP4728_H_
