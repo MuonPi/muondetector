2e80992995b428e5196b8a377e1d1391fd6d451c
marvin5300 <37504638+marvin5300@users.noreply.github.com>
2026-04-15 01:44:04 +0200

    Ublox now not depending on qt anymore Rewritten ublox module.

---
+++ daemon/src/hardware/ublox/message_processor.h
@@ -0,0 +1,51 @@
+#ifndef MESSAGE_PROCESSOR_H
+#define MESSAGE_PROCESSOR_H
+
+#include "data/ublox/ublox_messages.h"
+#include "data/ublox/ublox_structs.h"
+#include <functional>
+#include <optional>
+#include <string>
+#include <unordered_map>
+
+class MessageProcessor
+{
+  public:
+    MessageProcessor() = delete;
+    ~MessageProcessor() = delete;
+
+    // all functions only used for processing and showing "UbxMessage"
+    static auto processMessage(const UbxMessage &msg) -> std::optional<UbxEvent>;
+
+    static const std::unordered_map<UBX_MSG::msg_id, std::pair<std::function<std::optional<UbxEvent>()>, std::string>> handler;
+
+
+  private:
+    static void unhandled(const UbxMessage &msg, const std::map<std::uint8_t, const char *> ubx_class_names);
+    static auto UBXNavStatus(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXNavDOP(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXNavTimeGPS(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXNavTimeUTC(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXNavClock(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXNavSVinfo(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXNavSat(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXNavPosLLH(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXCfgAnt(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXCfgNavX5(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXCfgNav5(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXCfgTP5(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXCfgGNSS(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXCfgMSG(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXMonRx(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXMonTx(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXMonHW(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXMonHW2(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXMonVer(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXTimTP(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto UBXTimTM2(const std::string& msg) -> std::optional<UbxEvent>;
+    static auto getProtVersion(const std::string& prot) -> double;
+    static std::optional<GpsVersion> gpsVersion;
+    static std::optional<UbxMessage> msgWaitingForAck;
+};
+
+#endif // MESSAGE_PROCESSOR_H
--- library/src/data/ublox/ublox_structs.h
+++ library/src/data/ublox/ublox_structs.h
@@ -7,8 +7,10 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <variant>
 
 namespace Gnss {
+
 
 struct Id {
     enum {
@@ -89,6 +91,10 @@ struct SvHealth {
 
 } // namespace Gnss
 
+
+constexpr std::size_t s_nr_targets { 6 };
+constexpr std::size_t s_default_target { 1 };
+
 struct UbxMessage {
     UbxMessage() = default;
     UbxMessage(std::uint16_t msg_id, const std::string a_payload) noexcept;
@@ -106,27 +112,18 @@ private:
     std::string m_payload {};
 };
 
-struct GnssPosStruct {
-    uint32_t iTOW;
-    int32_t lon; // longitude 1e-7 scaling (increase by 1 means 100 nano degrees)
-    int32_t lat; // latitude 1e-7 scaling (increase by 1 means 100 nano degrees)
-    int32_t height; // height above ellipsoid
-    int32_t hMSL; // height above main sea level
-    uint32_t hAcc; // horizontal accuracy estimate
-    uint32_t vAcc; // vertical accuracy estimate
-};
 
 struct GnssConfigStruct {
-    uint8_t gnssId;
-    uint8_t resTrkCh;
-    uint8_t maxTrkCh;
-    uint32_t flags;
+    std::uint8_t gnssId;
+    std::uint8_t resTrkCh;
+    std::uint8_t maxTrkCh;
+    std::uint32_t flags;
 };
 
 class GnssSatellite {
 public:
     GnssSatellite() = default;
-    GnssSatellite(int gnssId, int satId, int cnr, int elev, int azim, float prRes, uint32_t flags)
+    GnssSatellite(int gnssId, int satId, int cnr, int elev, int azim, float prRes, std::uint32_t flags)
         : GnssId(gnssId)
         , SatId(satId)
         , Cnr(cnr)
@@ -173,77 +170,19 @@ public:
         return sat1.Cnr > sat2.Cnr;
     }
 
-    // friend QDataStream& operator<<(QDataStream& out, const GnssSatellite& sat);
-    // friend QDataStream& operator>>(QDataStream& in, GnssSatellite& sat);
-
 public:
-    uint8_t GnssId { 0 };
-    uint8_t SatId { 0 };
-    uint8_t Cnr { 0 };
+    std::uint8_t GnssId { 0 };
+    std::uint8_t SatId { 0 };
+    std::uint8_t Cnr { 0 };
     int8_t Elev { 0 };
     int16_t Azim { 0 };
     float PrRes { 0. };
-    uint8_t Quality { 0 };
-    uint8_t Health { 0 };
-    uint8_t OrbitSource { 0 };
+    std::uint8_t Quality { 0 };
+    std::uint8_t Health { 0 };
+    std::uint8_t OrbitSource { 0 };
     bool Used { false };
     bool DiffCorr { false };
     bool Smoothed { false };
-};
-
-struct UbxTimePulseStruct {
-    enum { ACTIVE = 0x01,
-        LOCK_GPS = 0x02,
-        LOCK_OTHER = 0x04,
-        IS_FREQ = 0x08,
-        IS_LENGTH = 0x10,
-        ALIGN_TO_TOW = 0x20,
-        POLARITY = 0x40,
-        GRID_UTC_GPS = 0x780 };
-    uint8_t tpIndex = 0;
-    uint8_t version = 0;
-    int16_t antCableDelay = 0;
-    int16_t rfGroupDelay = 0;
-    uint32_t freqPeriod = 0;
-    uint32_t freqPeriodLock = 0;
-    uint32_t pulseLenRatio = 0;
-    uint32_t pulseLenRatioLock = 0;
-    int32_t userConfigDelay = 0;
-    uint32_t flags = 0;
-};
-
-struct UbxTimeMarkStruct {
-    enum { TIMEBASE_LOCAL = 0x00,
-        TIMEBASE_GNSS = 0x01,
-        TIMEBASE_UTC = 0x02,
-        TIMEBASE_OTHER = 0x03 };
-    struct timespec rising = { 0, 0 };
-    struct timespec falling = { 0, 0 };
-    bool risingValid = false;
-    bool fallingValid = false;
-    uint32_t accuracy_ns = 0;
-    bool valid = false;
-    uint8_t timeBase = 0;
-    bool utcAvailable = false;
-    uint8_t flags = 0;
-    uint16_t evtCounter = 0;
-};
-
-struct GnssMonHwStruct {
-    uint16_t noise { 0 };
-    uint16_t agc { 0 };
-    uint8_t antStatus { 0 };
-    uint8_t antPower { 0 };
-    uint8_t jamInd { 0 };
-    uint8_t flags { 0 };
-};
-
-struct GnssMonHw2Struct {
-    int8_t ofsI { 0 };
-    int8_t ofsQ { 0 };
-    uint8_t magI { 0 };
-    uint8_t magQ { 0 };
-    uint8_t cfgSrc { 0 };
 };
 
 struct gpsTimestamp {
@@ -270,40 +209,6 @@ enum UbxDynamicModel {
     bike = 10
 };
 
-template <typename T>
-class gpsProperty {
-public:
-    gpsProperty()
-        : value()
-    {
-        updated = false;
-    }
-    gpsProperty(const T& val)
-    {
-        value = val;
-        updated = true;
-        lastUpdate = std::chrono::system_clock::now();
-    }
-    std::chrono::time_point<std::chrono::system_clock> lastUpdate;
-    std::chrono::duration<double> updatePeriod;
-    std::chrono::duration<double> updateAge() { return std::chrono::system_clock::now() - lastUpdate; }
-    bool updated;
-    gpsProperty& operator=(const T& val)
-    {
-        value = val;
-        lastUpdate = std::chrono::system_clock::now();
-        updated = true;
-        return *this;
-    }
-    const T& operator()()
-    {
-        updated = false;
-        return value;
-    }
-
-private:
-    T value;
-};
 
 struct UbxDopStruct {
     uint16_t gDOP = 0, pDOP = 0, tDOP = 0, vDOP = 0, hDOP = 0, nDOP = 0, eDOP = 0;
@@ -350,5 +255,252 @@ inline void GnssSatellite::Print(int index, bool wHeader) const
     ;
     std::cout << std::endl;
 }
+
+struct NavStatus
+{
+    std::uint32_t iTOW{0};
+    std::uint8_t gpsFix{0};
+    std::uint8_t flags{0};
+    std::uint8_t flags2{0};
+    std::uint32_t ttff{0};
+    std::uint32_t msss{0};
+};
+
+struct NavTimeGPS
+{
+    std::uint32_t iTOW{0};
+    std::int32_t fTOW{0};
+    std::uint16_t wnR{0};
+    std::int8_t leapS{0};
+    std::uint8_t flags{0};
+
+    // time accuracy estimate
+    std::uint32_t tAcc{0};
+};
+
+struct NavTimeUTC
+{
+    std::uint32_t iTOW{0};
+    std::uint32_t tAcc{0};
+    std::int32_t nano{0};
+    std::uint16_t year{0};
+    std::uint16_t month{0};
+    std::uint16_t day{0};
+    std::uint16_t hour{0};
+    std::uint16_t min{0};
+    std::uint16_t sec{0};
+    std::uint8_t flags{0};
+};
+
+struct NavClock
+{
+    std::uint32_t iTOW{0};
+    // clock bias
+    std::int32_t clkB{0};
+    // clock drift
+    std::int32_t clkD{0};
+    std::uint32_t tAcc{0};
+    std::uint32_t fAcc{0};
+};
+
+struct NavSVinfo
+{
+    std::uint32_t iTOW{0};
+    std::uint8_t numSvs{0};
+    std::uint8_t globFlags{0};
+    std::size_t goodSats{0};
+    std::vector<GnssSatellite> satellites{0};
+};
+
+struct NavSat
+{
+    std::uint32_t iTOW{0};
+    std::optional<std::uint8_t> version{std::nullopt};
+    std::optional<std::uint8_t> globFlags{std::nullopt};
+    std::uint8_t numSvs{0};
+    std::size_t goodSats{0};
+    std::vector<GnssSatellite> satellites{0};
+};
+
+struct GnssPosStruct {
+    std::uint32_t iTOW{0};
+    std::int32_t lon{0}; // longitude 1e-7 scaling (increase by 1 means 100 nano degrees)
+    std::int32_t lat{0}; // latitude 1e-7 scaling (increase by 1 means 100 nano degrees)
+    std::int32_t height{0}; // height above ellipsoid
+    std::int32_t hMSL{0}; // height above main sea level
+    std::uint32_t hAcc{0}; // horizontal accuracy estimate
+    std::uint32_t vAcc{0}; // vertical accuracy estimate
+};
+
+struct CfgAnt
+{
+    std::uint16_t flags{0};
+    std::uint16_t pins{0};
+};
+
+struct CfgNavX5
+{
+    std::uint8_t version{0};
+    // std::uint16_t mask1{0};
+    std::uint8_t minSVs{0};
+    std::uint8_t maxSVs{0};
+    std::uint8_t minCNO{0};
+    std::uint8_t iniFix3D{0};
+    std::uint16_t wknRollover{0};
+    std::uint8_t aopCfg{0};
+    std::uint16_t aopOrbMaxErr{0};
+};
+
+struct CfgNav5
+{
+    std::uint16_t mask{0};
+    std::uint8_t dynModel{0};
+    std::uint8_t fixMode{0};
+    std::int32_t fixedAlt{0};
+    std::uint32_t fixedAltVar{0};
+    std::int8_t minElev{0};
+    std::uint8_t cnoThreshNumSVs{0};
+    std::uint8_t cnoThresh{0};
+};
+
+struct UbxTimePulseStruct
+{
+    enum { ACTIVE = 0x01,
+        LOCK_GPS = 0x02,
+        LOCK_OTHER = 0x04,
+        IS_FREQ = 0x08,
+        IS_LENGTH = 0x10,
+        ALIGN_TO_TOW = 0x20,
+        POLARITY = 0x40,
+        GRID_UTC_GPS = 0x780 };
+    std::uint8_t tpIndex{0};
+    std::uint8_t version{0};
+    int16_t antCableDelay{0};
+    int16_t rfGroupDelay{0};
+    std::uint32_t freqPeriod{0};
+    std::uint32_t freqPeriodLock{0};
+    std::uint32_t pulseLenRatio{0};
+    std::uint32_t pulseLenRatioLock{0};
+    std::int32_t userConfigDelay{0};
+    std::uint32_t flags{0};
+};
+
+struct CfgGNSS
+{
+
+    std::uint8_t version{0};
+    std::uint8_t numTrkChHw{0};
+    std::uint8_t numTrkChUse{0};
+    std::uint8_t numConfigBlocks{0};
+    std::vector<GnssConfigStruct> configs;
+};
+
+struct CfgMsg
+{
+    std::uint16_t msgID{0};
+    std::uint8_t rate{0};
+};
+
+struct MonRx
+{
+    std::array<std::uint16_t, s_nr_targets> pending{};
+    std::array<std::uint8_t, s_nr_targets> usage{};
+    std::array<std::uint8_t, s_nr_targets> peakUsage{};
+    std::uint8_t tUsage{0};
+    std::uint8_t tPeakUsage{0};
+};
+
+
+struct MonTx
+{
+    std::array<std::uint16_t, s_nr_targets> pending{};
+    std::array<std::uint8_t, s_nr_targets> usage{};
+    std::array<std::uint8_t, s_nr_targets> peakUsage{};
+    std::uint8_t tUsage{0};
+    std::uint8_t tPeakUsage{0};
+};
+
+struct GnssMonHwStruct
+{
+    std::uint16_t noisePerMS{0};
+    std::uint16_t agcCnt{0};
+    std::uint8_t antStatus{0};
+    std::uint8_t antPower{0};
+    std::uint8_t flags{0};
+    std::uint8_t jamInd{0};
+};
+
+struct GnssMonHw2Struct
+{
+    std::int8_t ofsI{0};
+    std::uint8_t magI{0};
+    std::int8_t ofsQ{0};
+    std::uint8_t magQ{0};
+    std::uint8_t cfgSrc{0};
+    std::uint32_t postStatus{0};
+};
+
+struct GpsVersion
+{
+    std::string hwString{0};
+    std::string swString{0};
+    std::string prot{0};
+};
+
+struct TimTP
+{
+
+    std::uint32_t towMS{0};
+    // TP time of week, sub ms
+    std::uint32_t towSubMS{0};
+    // quantization error
+    std::int32_t qErr{0};
+    // week number
+    std::uint16_t week{0};
+    // flags
+    std::uint8_t flags{0};
+    // ref info
+    std::uint8_t refInfo{0};
+};
+
+struct UbxTimeMarkStruct {
+    enum { TIMEBASE_LOCAL = 0x00,
+        TIMEBASE_GNSS = 0x01,
+        TIMEBASE_UTC = 0x02,
+        TIMEBASE_OTHER = 0x03 };
+    struct timespec rising = { 0, 0 };
+    struct timespec falling = { 0, 0 };
+    bool risingValid = false;
+    bool fallingValid = false;
+    std::uint32_t accuracy_ns = 0;
+    bool valid = false;
+    std::uint8_t timeBase = 0;
+    bool utcAvailable = false;
+    std::uint8_t flags = 0;
+    uint16_t evtCounter = 0;
+};
+
+using UbxEvent = std::variant<
+    NavStatus,
+    UbxDopStruct,
+    NavTimeGPS,
+    NavTimeUTC,
+    NavClock,
+    NavSat,
+    GnssPosStruct,
+    CfgAnt,
+    CfgNavX5,
+    CfgNav5,
+    UbxTimePulseStruct,
+    CfgGNSS,
+    CfgMsg,
+    MonRx,
+    MonTx,
+    GnssMonHwStruct,
+    GnssMonHw2Struct,
+    GpsVersion,
+    TimTP,
+    UbxTimeMarkStruct
+>;
 
 #endif // UBLOX_STRUCTS_H
