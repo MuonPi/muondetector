694c662a7426e938fd26cf4ee7fe32371710a9e8
Marvin Peter <37504638+marvin5300@users.noreply.github.com>
2026-06-08 13:58:11 +0200

    Update Actions

--- cmake/qwt.cmake
+++ cmake/qwt.cmake
@@ -72,15 +72,18 @@ else()
             OUTPUT ${QWT_BUILD_DIR}/lib/qwt.dll ${QWT_BUILD_DIR}/lib/libqwt.a
             COMMAND ${CMAKE_COMMAND} -E make_directory ${QWT_BUILD_DIR}
             COMMAND ${CMAKE_COMMAND} -E env
-                "PATH=C:/Qt/Tools/llvm-mingw1706_64/bin;$ENV{PATH}"
+                PATH=C:/Qt/Tools/llvm-mingw1706_64/bin;$ENV{PATH}
                 QMAKESPEC=win32-clang-g++
+                QMAKE_CC=C:/Qt/Tools/llvm-mingw1706_64/bin/clang.exe
+                QMAKE_CXX=C:/Qt/Tools/llvm-mingw1706_64/bin/clang++.exe
 
                 ${QMAKE_EXECUTABLE}
                 ${qwt_SOURCE_DIR}/qwt.pro
                 CONFIG+=release
 
-
-            COMMAND "${MINGW_MAKE}" -j ${CPU_CORE_SUFFIX}
+            COMMAND ${CMAKE_COMMAND} -E env
+                PATH=C:/Qt/Tools/llvm-mingw1706_64/bin;$ENV{PATH}
+                ${MINGW_MAKE} -j 24
 
             WORKING_DIRECTORY ${QWT_BUILD_DIR}
             DEPENDS ${qwt_SOURCE_DIR}
